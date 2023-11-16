a5afb31652b348176f004de79b519b424d7245d1
Benoît Legat <benoit.legat@gmail.com>
2023-11-16 11:41:02 +0100

    Add Parameters struct (#6)
    * Add Parameters struct
    
    * Fix kwdef
    
    * Fix

--- src/SDPLR.jl
+++ src/SDPLR.jl
@@ -6,6 +6,24 @@ function solve_sdpa_file(file)
     return run(`$(SDPLR_jll.sdplr()) $file`)
 end
 
+# Default values taken from `SDPLR-1.03-beta/source/params.c`
+Base.@kwdef struct Parameters
+    #inputtype = 1
+    rho_f::Cdouble = 1.0e-5
+    rho_c::Cdouble = 1.0e-1
+    sigmafac::Cdouble = 2.0
+    rankreduce::Csize_t = 0
+    timelim::Csize_t = 3600
+    printlevel::Csize_t = 1
+    dthresh_dim::Csize_t = 10
+    dthresh_dens::Cdouble = 0.75
+    numbfgsvecs::Csize_t = 4
+    rankredtol::Cdouble = 2.2204460492503131e-16
+    gaptol::Cdouble = 1.0e-3
+    checkbd::Cptrdiff_t = -1
+    typebd::Cptrdiff_t = 1
+end
+
 function solve(
     blksz::Vector{Cptrdiff_t},
     blktype::Vector{Cchar},
@@ -15,19 +33,7 @@ function solve(
     CAcol::Vector{Csize_t},
     CAinfo_entptr::Vector{Csize_t},
     CAinfo_type::Vector{Cchar},
-    numbfgsvecs::Integer,
-    rho_f::Cdouble,
-    rho_c::Cdouble,
-    sigmafac::Cdouble,
-    rankreduce::Integer,
-    gaptol::Cdouble,
-    checkbd::Integer,
-    typebd::Integer,
-    dthresh_dim::Integer,
-    dthresh_dens::Cdouble,
-    timelim::Integer,
-    rankredtol::Cdouble,
-    printlevel::Integer,
+    params::Parameters,
     R::Vector{Cdouble},
     lambda::Vector{Cdouble},
     maxranks::Vector{Csize_t},
@@ -55,19 +61,19 @@ function solve(
         CAcol::Ptr{Csize_t},
         CAinfo_entptr::Ptr{Csize_t},
         CAinfo_type::Ptr{Cchar},
-        numbfgsvecs::Csize_t,
-        rho_f::Cdouble,
-        rho_c::Cdouble,
-        sigmafac::Cdouble,
-        rankreduce::Csize_t,
-        gaptol::Cdouble,
-        checkbd::Cptrdiff_t,
-        typebd::Csize_t,
-        dthresh_dim::Csize_t,
-        dthresh_dens::Cdouble,
-        timelim::Csize_t,
-        rankredtol::Cdouble,
-        printlevel::Csize_t,
+        params.numbfgsvecs::Csize_t,
+        params.rho_f::Cdouble,
+        params.rho_c::Cdouble,
+        params.sigmafac::Cdouble,
+        params.rankreduce::Csize_t,
+        params.gaptol::Cdouble,
+        params.checkbd::Cptrdiff_t,
+        params.typebd::Csize_t,
+        params.dthresh_dim::Csize_t,
+        params.dthresh_dens::Cdouble,
+        params.timelim::Csize_t,
+        params.rankredtol::Cdouble,
+        params.printlevel::Csize_t,
         R::Ptr{Cdouble},
         lambda::Ptr{Cdouble},
         maxranks::Ptr{Csize_t},
---
+++ test/test_vibra.jl
@@ -0,0 +1,25 @@
+using Test
+import SDPLR
+@testset "Solve vibra with sdplr executable" begin
+    SDPLR.solve_sdpa_file("vibra1.dat-s")
+end
+@testset "Solve vibra with sdplrlib" begin
+    include("vibra.jl")
+    ret = SDPLR.solve(
+        blksz,
+        blktype,
+        b,
+        CAent,
+        CArow,
+        CAcol,
+        CAinfo_entptr,
+        CAinfo_type,
+        SDPLR.Parameters(),
+        R,
+        lambda,
+        maxranks,
+        ranks,
+        pieces,
+    )
+    @test iszero(ret)
+end
