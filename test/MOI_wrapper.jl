677bf6716932d9db145c4d42acf12c363f82a544
odow <o.dowson@gmail.com>
2025-04-03 16:17:18 +1300

    Add license headers to Julia files

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2017: Benoît Legat and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module TestMOI
 
 using Test
