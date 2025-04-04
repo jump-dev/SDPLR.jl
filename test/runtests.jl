93eed79dbf7154d5de827b446d3f48b1a0e86754
Benoît Legat <benoit.legat@gmail.com>
2025-04-04 16:19:26 +0200

    Add LRO tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,16 +13,9 @@ import Random
 import SDPLR
 
 function test_runtests()
-    optimizer = SDPLR.Optimizer()
-    MOI.set(optimizer, MOI.Silent(), true)
-    MOI.set(optimizer, MOI.RawOptimizerAttribute("timelim"), 10)
-    model = MOI.Bridges.full_bridge_optimizer(
-        MOI.Utilities.CachingOptimizer(
-            MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-            optimizer,
-        ),
-        Float64,
-    )
+    model = MOI.instantiate(SDPLR.Optimizer, with_bridge_type = Float64, with_cache_type = Float64)
+    MOI.set(model, MOI.Silent(), true)
+    MOI.set(model, MOI.RawOptimizerAttribute("timelim"), 10)
     config = MOI.Test.Config(
         rtol = 1e-1,
         atol = 1e-1,
@@ -49,6 +42,30 @@ function test_runtests()
             # Incorrect `ConstraintDual` for `vc2` for MacOS in CI
             r"test_linear_integration$",
         ],
+    )
+    return
+end
+
+function test_LRO_runtests()
+    model = MOI.instantiate(SDPLR.Optimizer, with_bridge_type = Float64, with_cache_type = Float64)
+    MOI.set(model, MOI.Silent(), true)
+    MOI.set(model, MOI.RawOptimizerAttribute("timelim"), 10)
+    config = MOI.Test.Config(
+        rtol = 1e-1,
+        atol = 1e-1,
+        exclude = Any[
+            MOI.ConstraintBasisStatus,
+            MOI.VariableBasisStatus,
+            MOI.ConstraintName,
+            MOI.VariableName,
+            MOI.ObjectiveBound,
+            MOI.SolverVersion,
+        ],
+        optimal_status = MOI.LOCALLY_SOLVED,
+    )
+    LRO.Test.runtests(
+        model,
+        config,
     )
     return
 end
